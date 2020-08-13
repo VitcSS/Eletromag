310336b1c767ecccbff047fcac21ed75500831b4
Henrique <henrique2204barbosa@gmail.com>
2020-08-13 15:34:23 -0300

    m

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+window.minsize(1200, 600)
+
+canvas = tk.Canvas(window)
+canvas.pack()
+
+Label(window, text = 'Simulação Campos',
+      font =('Verdana', 30)).pack(side = BOTTOM)
+
+canvas.create_line(10, 10, 200, 100, arrow=tk.LAST, fill='blue')
+canvas.create_line(50, 50, 100, 50, arrow=tk.LAST, fill='red')
+
+window.mainloop()
